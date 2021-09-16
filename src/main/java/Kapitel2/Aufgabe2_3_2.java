a906f3b06f18bd2ca8264eb3e2100367bcfea385
OlafKulczycki <88832369+OlafKulczycki@users.noreply.github.com>
2021-09-16 21:07:42 +0200

    Klassen mit denen ich die Aufgaben gecodet habe.

--- src/main/java/Kapitel2/Aufgabe2_3_2.java
+++ src/main/java/Kapitel2/Aufgabe2_3_2.java
@@ -1,14 +1,25 @@
 package Kapitel2;
 
+//Das Programm fragt, wie viele Rumflaschen erbeutet wurden und wie großm die Crew ist.
+// Der Captain bekommt immer die Hälfte der Flaschen und der Rest wird unter der Crew aufgeteilt.
+
 public class Aufgabe2_3_2 {
     //Methoden
 
     public static void main(String[] args) {
-    //Variablen
+        //Variablen
+        boolean fair = false;
 
-    //Befehle
-        System.out.printf("Aye du Landratte, wie viele Flaschen Rum habt Ihr erbeitet ?\n");
-    int bottles = new java.util.Scanner(System.in).nextInt();
+        //Befehle
+        System.out.printf("Aye du Landratte, wie viele Flaschen Rum habt Ihr erbeutet ?\n");
+        int bottles = new java.util.Scanner(System.in).nextInt();
+        System.out.printf("Wie viele Mitglieder hat unsere Crew gerade ?\n");
+        int crew = new java.util.Scanner(System.in).nextInt();
+        System.out.printf("ARRRR..... " + bottles + " Flaschen Rum, nicht schlecht.\nDer Captain bekommt demnach " +
+                (bottles / 2) + " Pullen Rum.");
+        System.out.printf("\nBei der " + crew + " Mann starken Crew bekommt demnach jeder " + ((bottles / 2) / crew) +
+                " Pullen Rum.");
+
 
     }
 }
